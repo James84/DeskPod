64fbb4d8373601c22eb3f72eddae08890df219a3
James Runham <james.runham@reedonline.co.uk>
2017-04-21 15:08:29 +0100

    updated podcast service

---
+++ DeskPod/DeskPod.Web/Models/PodcastModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DeskPod.Web.Models
+{
+    public class PodcastModel
+    {
+        public string Title { get; }
+        public string Url { get; }
+        public DateTime PublicationDate { get; set; }
+
+        public PodcastModel(string title, string url, DateTime publicationDate)
+        {
+            Title = title;
+            Url = url;
+            PublicationDate = publicationDate;
+        }
+    }
+}
---
+++ DeskPod/DeskPod.Web/Models/PodcastViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+namespace DeskPod.Web.Models
+{
+    public class PodcastViewModel
+    {
+        public IList<PodcastModel> Podcasts { get; set; }
+
+        public PodcastViewModel()
+        {
+            Podcasts = new List<PodcastModel>();
+        }
+    }
+}
